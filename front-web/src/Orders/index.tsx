584763fffd7379fa3831e92dd0d14f606d8b5876
Fernanda Uchoa <nanda.uq015@gmail.com>
2021-01-10 12:58:08 -0300

    Adicionando Routes e StepsHeaders

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import StepsHeaders from './StepsHeaders';
+import './styles.css';
+
+function Orders(){
+    return(
+        <div className="orders-container">
+            <StepsHeaders/>
+        </div>
+    )
+}
+
+export default Orders;

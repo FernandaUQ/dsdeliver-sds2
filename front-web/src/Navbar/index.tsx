b30cffdb6e34027ac853f67943e6d8be241537b3
Fernanda Uchoa <nanda.uq015@gmail.com>
2021-01-10 01:39:07 -0300

    Adicionando Navbar

---
+++ front-web/src/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { ReactComponent as Logo} from './logo.svg';
+
+function Navbar (){
+    return(
+        <nav className="main-navbar">
+            <Logo />
+            <a href="home" className="logo-text">DSDelivery</a>
+        </nav>
+    )
+}
+export default Navbar;
